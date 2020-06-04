05c41eeda37b7dc45450281088e0e8b4e14af9ed
LuisEduJulio <luiseduardoer38@gmail.com>
2020-06-03 23:44:37 -0300

    Adicionando database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    res.json([
-        'Luis',
-        'Marcos'
-    ])
-})
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333);
